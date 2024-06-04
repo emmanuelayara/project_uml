405e741091ddfde500d40077ac234acd038f159a
emmanuelayara <ayaraemmanuel16@gmail.com>
2024-06-04 13:08:14 +0100

    corrected database

--- models.py
+++ models.py
@@ -12,77 +12,75 @@ class Users(db.Model):
   first_name = db.Column(db.String(500))
   last_name = db.Column(db.String(500))
   role = db.Column(db.String(500))
-  year_of_graduation = db.column(db.varchar(500))
+  year_of_graduation = db.column(db.integer(500))
   previous_employment = db.Column(db.String(500))
 
   # Create employer table
-class employers(db.Model):
+class Employers(db.Model):
   __tablename__ = 'employers'
 
-  company_id = db.column(db.int, primary_key=True)
+  company_id = db.column(db.Integer, primary_key=True)
   email = db.Column(db.String,)
   password = db.Column(db.String(500))
   company_name = db.Column(db.String(500))
-  location = db.column(db.varchar(500))
+  location = db.column(db.String(500))
   available_role = db.Column(db.String(500))
-  website = db.column(db.varchar(500))
+  website = db.column(db.String(500))
 
 
     # Create profile table
-class profiles(db.Model):
+class Profiles(db.Model):
   __tablename__ = 'profile'
 
-  profile_id = db.column(db.int, primary_key=True)
-  user_id = db.column(db.int, foreign_key=True)
-  bio = db.column(db.string) 
-  location = db.column(db.varchar)
-  skills = db.column(db.string)
+  profile_id = db.column(db.integer, primary_key=True)
+  user_id = db.column(db.integer, foreign_key=True)
+  bio = db.column(db.String) 
+  location = db.column(db.String)
+  skills = db.column(db.String)
 
 
     # Create job table
-class jobs(db.Model):
+class Jobs(db.Model):
   __tablename__ = 'job'
 
-  job_id = db.column(db.int, primary_key=True)
-  company_id = db.column(db.int, foreign_key=True)
-  title = db.column(db.varchar(100))
-  description = db.column(db.varchar(500))
-  location = db.column(db.varchar(500))
-  salary = db.column(db.varchar(500))
-  employer_id = db.column(db.int, foreign_key=True)
-  job_type = db.column(db.varchar(job_type in ("full time", "part time", "contract", "internship")))
+  job_id = db.column(db.integer, primary_key=True)
+  company_id = db.column(db.integer, foreign_key=True)
+  title = db.column(db.String(100))
+  description = db.column(db.String(500))
+  location = db.column(db.String(500))
+  salary = db.column(db.String(500))
+  employer_id = db.column(db.integer, foreign_key=True)
+  job_type = db.column(db.String(job_type in ("full time", "part time", "contract", "internship")))
 
 
     # Create application table
-class applications(db.Model):
+class Applications(db.Model):
   __tablename__ = 'application'
 
-application_id = db.column(db.int, primary_key=True)
-user_id = db.column(db.int, foreign_key=True)
-job_id = db.column(db.int, foreign_key=True)
-application_date = db.column (db.datetime)
-status = db.column (db.text(status in ("applied", "under review", "interview", "offered", "rejected", "withdrawn")))
+application_id = db.column(db.integer, primary_key=True)
+user_id = db.column(db.integer, foreign_key=True)
+job_id = db.column(db.integer, foreign_key=True)
+status = db.column (db.String(status in ("applied", "under review", "interview", "offered", "rejected", "withdrawn")))
 
 
     # Create notification table
-class notifications(db.Model):
+class Notifications(db.Model):
   __tablename__ = 'notification'
 
-  notification_id = db.column(db.int, primary_key=True)
-  user_id = db.column(db.int, foreign_key=True)
-  message = db.column(db.text)
-  date = db.column(db.datetime)
+  notification_id = db.column(db.integer, primary_key=True)
+  user_id = db.column(db.integer, foreign_key=True)
+  message = db.column(db.String)
   is_read = db.column(db.boolean(False))
 
 
     # Admin table
-class admin(db.Model):
+class Admin(db.Model):
     __tablename__ = 'admin'
 
-    action_id = db.column(db.int, primary_key=True)
-    admin_id = db.column(db.int, foreign_key=True)
-    action_type = db.column(db.varchar(100))
-    target_id = db.column(db.int)
+    action_id = db.column(db.integer, primary_key=True)
+    admin_id = db.column(db.integer, foreign_key=True)
+    action_type = db.column(db.String(100))
+    target_id = db.column(db.integer)
     date = db.column(db.datetime)
 
 
