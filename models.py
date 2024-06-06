3f13ad2b166391002c8dd2399f9371da1ac86895
Faith Ikhidero <ikhidero.faithy@gmail.com>
2024-06-06 09:41:00 +0100

    made changes to the app setup and models

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 
@@ -12,75 +13,97 @@ class Users(db.Model):
   first_name = db.Column(db.String(500))
   last_name = db.Column(db.String(500))
   role = db.Column(db.String(500))
-  year_of_graduation = db.column(db.integer(500))
+  year_of_graduation = db.Column(db.Integer())
   previous_employment = db.Column(db.String(500))
+  notifications = db.relationship('Notifications', backref='notifications', lazy=True)
+  profiles = db.relationship('Profiles', backref='profile', lazy=True)
+  applications = db.relationship('Applications', backref='applications', lazy=True)
+
+  def __repr__(self):
+    return f"<Users {self.name}>"
 
   # Create employer table
 class Employers(db.Model):
   __tablename__ = 'employers'
 
-  company_id = db.column(db.Integer, primary_key=True)
+  company_id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String,)
   password = db.Column(db.String(500))
   company_name = db.Column(db.String(500))
-  location = db.column(db.String(500))
+  location = db.Column(db.String(500))
   available_role = db.Column(db.String(500))
-  website = db.column(db.String(500))
+  website = db.Column(db.String(500))
+  jobs = db.relationship('Jobs', backref='jobs', lazy=True)
+
+  def __repr__(self):
+    return f"<Employers {self.name}>"
 
 
     # Create profile table
 class Profiles(db.Model):
-  __tablename__ = 'profile'
+  __tablename__ = 'profiles'
 
-  profile_id = db.column(db.integer, primary_key=True)
-  user_id = db.column(db.integer, foreign_key=True)
-  bio = db.column(db.String) 
-  location = db.column(db.String)
-  skills = db.column(db.String)
+  profile_id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.email'), nullable=False)
+  bio = db.Column(db.String) 
+  location = db.Column(db.String)
+  skills = db.Column(db.String)
+
+  def __repr__(self):
+    return f"<Profiles {self.name}>"
 
 
     # Create job table
 class Jobs(db.Model):
-  __tablename__ = 'job'
+  __tablename__ = 'jobs'
 
-  job_id = db.column(db.integer, primary_key=True)
-  company_id = db.column(db.integer, foreign_key=True)
-  title = db.column(db.String(100))
-  description = db.column(db.String(500))
-  location = db.column(db.String(500))
-  salary = db.column(db.String(500))
-  employer_id = db.column(db.integer, foreign_key=True)
-  job_type = db.column(db.String(job_type in ("full time", "part time", "contract", "internship")))
+  job_id = db.Column(db.Integer, primary_key=True)
+  company_id = db.Column(db.Integer, db.ForeignKey('employers.company_id'), nullable=False)
+  title = db.Column(db.String(100))
+  description = db.Column(db.String(500))
+  location = db.Column(db.String(500))
+  salary = db.Column(db.String(500))
+  job_type = db.Column(db.String(500))
+  applications = db.relationship('Applications', backref='applications', lazy=True)
 
+  def __repr__(self):
+    return f"<Jobs {self.name}>"
 
     # Create application table
 class Applications(db.Model):
-  __tablename__ = 'application'
+  __tablename__ = 'applications'
 
-application_id = db.column(db.integer, primary_key=True)
-user_id = db.column(db.integer, foreign_key=True)
-job_id = db.column(db.integer, foreign_key=True)
-status = db.column (db.String(status in ("applied", "under review", "interview", "offered", "rejected", "withdrawn")))
+  application_id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.email'), nullable=False)
+  job_id = db.Column(db.Integer, db.ForeignKey('jobs.job_id'), nullable=False)
+  status = db.Column (db.String(500))
+
+  def __repr__(self):
+    return f"<Applications {self.name}>"
 
 
     # Create notification table
 class Notifications(db.Model):
-  __tablename__ = 'notification'
+  __tablename__ = 'notifications'
 
-  notification_id = db.column(db.integer, primary_key=True)
-  user_id = db.column(db.integer, foreign_key=True)
-  message = db.column(db.String)
-  is_read = db.column(db.boolean(False))
+  notification_id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.email'), nullable=False)
+  message = db.Column(db.String)
+  is_read = db.Column(db.Boolean(False))
+
+  def __repr__(self):
+    return f"<Notifications {self.name}>"
 
 
     # Admin table
 class Admin(db.Model):
     __tablename__ = 'admin'
 
-    action_id = db.column(db.integer, primary_key=True)
-    admin_id = db.column(db.integer, foreign_key=True)
-    action_type = db.column(db.String(100))
-    target_id = db.column(db.integer)
-    date = db.column(db.datetime)
+    admin_id = db.Column(db.Integer, primary_key=True)
+    action_id = db.Column(db.Integer)
+    action_type = db.Column(db.String(100))
+    target_id = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
 
-
+    def __repr__(self):
+      return f"<Admin {self.name}>"
