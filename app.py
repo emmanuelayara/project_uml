586a39c4f8925cda8be0c4a323b4b16a69175726
emmanuelayara <ayaraemmanuel16@gmail.com>
2024-06-27 22:01:20 +0100

    commit

--- app.py
+++ app.py
@@ -8,28 +8,29 @@ from models import Users, Applications, Jobs, Notifications, Employers, Profiles
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def newwave():
     return render_template ("newwave.html")
 
 
-@app.route("/feed")
+@app.route("/feed", methods=['GET'])
 def feed():
-    return render_template ("feed.html")    
+        if request.method == "GET":
+           return render_template ("feed.html")    
 
 @app.route("/jobs", methods=['GET'])
 def view_jobs():
     return render_template ("jobs.html")
 
-@app.route("/employee_login")
+@app.route("/employee_login", methods=['GET'])
 def employee_login():
     form = EmployeeLoginForm()
-    if request.method=="POST":
-        print("Username:",form.username.data)
-        print("Password:",form.password.data)
-    return render_template ("employee_login.html", form=form)
+    if request.method=="GET":
+       print("Username:",form.username.data)
+       print("Password:",form.password.data)
+       return render_template ("employee_login.html", form=form)
 
-@app.route("/employer_login")
+@app.route("/employer_login", methods=['GET'])
 def employer_login():
     return render_template ("employer_login.html")
 
@@ -37,7 +38,7 @@ def employer_login():
 @app.route("/employee_register", methods=["GET","POST"])
 def employee_register():
     if request.method == "GET":
-        return render_template ("employee_register.html")
+       return render_template ("employee_register.html")
     elif request.method == "POST":
         company_id = random.randint(1000000, 1999999)
         employer_info = Employers(
@@ -55,7 +56,18 @@ def employee_register():
 
 @app.route("/employer_register", methods=["GET","POST"])
 def employer_register():
-    return render_template ("employer_register.html")
+        if request.method == "GET":
+           return render_template ("employer_register.html")
+        elif request.method == "POST":
+            company_id = random.randint(1000000, 1999999)
+            employer_info = Employers(
+                company_id = company_id,
+                password = request.json.get("password"),
+                email = request.json.get("email"),
+                company_name = request.json.get("company_name"),
+                )
+
+
 
 @app.route("/employee_profile")
 def employee_profile():
@@ -72,22 +84,22 @@ def create_employee_profile():
         print("College:",form.college.data)
     return render_template ("create_employee_profile.html", form=form)
 
-@app.route("/employer_profile")
+@app.route("/employer_profile", methods=['POST'])
 def employer_profile():
     return render_template ("employer_profile.html")
 
 
-@app.route("/create_employer_profile")
+@app.route("/create_employer_profile", methods=['POST'])
 def create_employer_profile():
     return render_template ("create_employer_profile.html")
 
 @app.route("/network")
 def network():
-    return render_template ("network.html")
+    return render_template ("network.html", methods=['GET','POST'])
 
 @app.route("/notifications")
 def notifications():
-    return render_template ("notifications.html")
+    return render_template ("notifications.html", methods=['GET'])
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", debug=True)
