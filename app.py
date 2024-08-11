99429d7772b0cda433cf2adb52b0a2d359e139d3
Faith Ikhidero <ikhidero.faithy@gmail.com>
2024-08-11 22:22:55 +0100

    latest updates

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def create_user_profile(email):
     form = UserForm()
 
     if request.method == "GET":
-        return render_template("employee_profile.html", form=form)
+        return render_template("create_employee_profile.html", form=form)
     
     elif request.method=="POST":
         user_data = Profiles(
@@ -158,15 +158,15 @@ def employer_register():
 
 @app.route("/employer_login", methods=['GET', 'POST'])
 def employer_login():
-    form = EmployerForm
+    form = EmployerForm()
     if request.method == "GET":
         return render_template ("employer_login.html", form=form)
 
     elif request.method=="POST":
-        company_id = form.username.data,
-        password = form.password.data
+        company_id= form.company_id.data,
+        password = form.password.data,
 
-        user = Users.query.get(company_id)
+        user = Employers.query.get(company_id)
         if user.password == password:
             return render_template("employer_profile.html", data=user)
         
@@ -175,19 +175,119 @@ def employer_login():
         return jsonify(msg=message)
 
 
-@app.route("/employer_profile", methods=['POST'])
-def employer_profile():
-    return render_template ("employer_profile.html")
+@app.route("/employer_profile", methods=['GET'])
+def employer_profile(company_id):
+    if request.method == "GET":
+        data = Employers.query.get(company_id)
+        return render_template ("employer_profile.html")
 
 
 @app.route("/create_employer_profile", methods=['POST'])
-def create_employer_profile():
-    return render_template ("create_employer_profile.html")
+def create_employer_profile(company_id):
+    form = EmployerForm()
+
+    if request.method == "GET":
+        return render_template("employer_profile.html", form=form)
+    
+    elif request.method=="POST":
+        Employer_data = Profiles(
+            profile_id = random.randint(1000, 10000),
+            company_id = form.company_id.data,
+            company_name = form.company_name.data,
+            website = form.website.data,
+            location = form.location.data,
+        )
+
+        db.session.add(Employer_data)
+        db.session.commit()
+
+        data = Employers.query.get(company_id)
+        return render_template ("employer_profile.html", data=data)
+    
+@app.route("/employer_profile/<string:company_id>/update", methods=['GET', 'PUT'])
+def update_employer_profile(company_id):
+    form = EmployerForm()
+    data = Employers.query.get(company_id)
+
+    if data:
+        if request.method == "GET":
+            return render_template("employer_profile", form=form,data=data)
+        
+        elif request.method =="PUT":
+            profile = Profiles(
+                location = form.location.data,
+            )
+
+            db.session.add(profile)
+            db.session.commit()
+
+            data = Employers.query.get(company_id)
+            return render_template("employer_profile.html", data=data)
+        else:
+            return jsonify({"StatusCode": 404, "Message": "User does not exist"})
+        
+@app.route("/employer_account/<string:company_id>/delete", methods=['GET', 'DELETE'])
+def delete_employer_account(company_id):
+    form = EmployerForm()
+    data = Employers.query.get(company_id)
+
+    if request.method == 'GET':
+        return render_template ("employer_login.html", form=form)
+    
+    elif request.method == 'GET':
+        password = form.password.data
+
+        if data.password == password:
+
+            db.session.delete(data)
+            db.session.commit()
+        return jsonify({"StatusCode": 200, "message": "Account deleted successfully"})    
 
 
 @app.route("/jobs", methods=['GET','POST'])
 def jobs():
     return render_template ("jobs.html")
+
+@app.route("/create_job", methods=['POST'])
+def create_job(job_id):
+    form = JobForm()
+
+    if request.method == "GET":
+        return render_template("jobs.html", form=form)
+    
+    elif request.method=="POST":
+        Job_data = Jobs(
+            job_id = random.randint(1000, 10000),
+            company_id = form.company_id.data,
+            company_name = form.company_name.data,
+            title = form.title.data,
+            location = form.location.data,
+            job_type = form.job_type.data,
+            salary = form.salary.data,
+        )
+
+        db.session.add(Job_data)
+        db.session.commit()
+
+        data = Jobs.query.get(job_id)
+        return render_template ("jobs.html", data=data)
+    
+@app.route("/available_job/<string:job_id>/delete", methods=['GET', 'DELETE'])
+def delete_available_job(job_id):
+    form = JobForm()
+    data = Jobs.query.get(job_id)
+
+    if request.method == 'GET':
+        return render_template ("jobs.html", form=form)
+    
+    elif request.method == 'GET':
+        password = form.password.data
+
+        if data.password == password:
+
+            db.session.delete(data)
+            db.session.commit()
+        return jsonify({"StatusCode": 200, "message": "Available Job deleted successfully"})
 
 @app.route("/network", methods=['GET','POST'])
 def network():
